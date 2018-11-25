605c799c31029ba23690641eb384e2e22d2af8ed
CAmper18 <45084295+CAmper18@users.noreply.github.com>
2018-11-25 18:52:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -54,7 +54,35 @@ client.on('message', function(message) {
     }
     }
     });
+const Eris = require("eris");
+var Acura = new Eris(process.env.BOT_TOKEN);
+Acura.on("ready", () => {
+    console.log("tm");
+    onstart();
+});
+Acura = "505075875771842571 ";
+nick = "Stylight";
+function onstart(){
+    var v1 = nick.split('');
+    var counter;
+    var i=0;
+varAcura 2 = ' ';
+  var Acura =   setInterval(function(){
 
+     if (i == v1.length){
+     clearInterval(Acura);
+     onstart()
+      return;
+     }
+     var v2 = v1[i];
+   Acura  += v2;
+    Acura.editNickname(Acura);
+ 
+        i++;
+
+     }, 1000);
+}
+Acura.connect();
 
 
 
