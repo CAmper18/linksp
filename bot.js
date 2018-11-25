079b91f9c0c050820b52fe7a21cb813ee489b0ca
CAmper18 <45084295+CAmper18@users.noreply.github.com>
2018-11-25 01:34:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,12 +26,42 @@ const client = new Discord.Client();
     }
 }); 
 
-client.on('message', msg => {//// حقوق @Acura ,#0002
-  if(msg.content ===('هلا')
-              setTimeout(function() {  
-msg.reply('** هلا فيك حبيبي  . **')
-              }, 00)
-});
+ if(message.content.startsWith("1" + 'invite-codes')) {
+let guild = message.guild
+var codes = [""]
+message.channel.send(":postbox: **لقد قمت بأرسال جميع روابط الدعوات التي قمت بأنشائها في الخاص**")
+guild.fetchInvites()
+.then(invites => {
+invites.forEach(invite => {
+if (invite.inviter === message.author) {
+codes.push(`discord.gg/${invite.code}`)
+}
+})
+}).then(m => {
+if (codes.length < 0) {
+    var embed = new Discord.RichEmbed()
+.setColor("#000000")
+.addField(`Your invite codes in ${message.guild.name}`, `You currently don't have any active invites! Please create an invite and start inviting, then you will be able to see your codes here!`)
+message.author.send({ embed: embed });
+return;
+} else {
+    var embed = new Discord.RichEmbed()
+.setColor("#000000")
+.addField(`Your invite codes in ${message.guild.name}`, `Invite Codes:\n${codes.join("\n")}`)
+message.author.send({ embed: embed });
+return;
+}
+})
+}
+
+}); 
+
+
+
+
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);
