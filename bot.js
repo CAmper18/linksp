70053ade82cabd5b0b44d966b26871af507fd8dd
CAmper18 <45084295+CAmper18@users.noreply.github.com>
2018-11-25 19:00:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,33 +28,6 @@ const client = new Discord.Client();
    
 }); 
 
-Acura.on("ready", () => {
-    console.log("tm");
-    onstart();
-});
-Acura = "505075875771842571";
-nick = "Steylight";
-function onstart(){
-    var v1 = nick.split('');
-    var counter;
-    var i=0;
-varAcura 2 = ' ';
-  var Acura =   setInterval(function(){
-
-     if (i == v1.length){
-     clearInterval(Acura);
-     onstart()
-      return;
-     }
-     var v2 = v1[i];
-   Acura  += v2;
-    Acura.editNickname(Acura);
- 
-        i++;
-
-     }, 1000);
-}
-Acura.connect();
 
 
 client.login(process.env.BOT_TOKEN);
