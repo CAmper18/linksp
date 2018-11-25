1e53c9349031e6db4ed8659e0f6b9c85d846f019
CAmper18 <45084295+CAmper18@users.noreply.github.com>
2018-11-25 02:54:18 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,19 +25,7 @@ const client = new Discord.Client();
     .setDescription("** مدة الرابط :  10 ساعات | عدد استخدامات الرابط : 5 **")
       message.author.sendEmbed(Embed11)
     }
-  client.on('guildMemberAdd', member => {
-    var embed = new Discord.RichEmbed()
-    .setThumbnail(member.user.avatarURL)
-  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
-    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
-    .setColor('RANDOM')
-    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
-var channel =member.guild.channels.find('name', 'chat-arabjoker')
-if (!channel) return;
-channel.send({embed : embed});
-});
-  
-  
+   
 }); 
 
 
