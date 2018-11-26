3e9ea22810e64f52711b22d3e03f2a5cdd2f4d48
CAmper18 <45084295+CAmper18@users.noreply.github.com>
2018-11-27 00:07:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -161,7 +161,7 @@ client.on('message', msg => {
   });
   client.on('message', msg => {
     if (msg.content === 'راك كبير') {
-      msg.reply('**زوجلي اختك **');
+      msg.reply('**الكبير ربي **');
     }
   });
 
@@ -172,21 +172,13 @@ client.on('message', msg => {
 
 
 
-var antispam = require("anti-spam");//npm i anti-spam
- 
-antispam(client, {
-  warnBuffer: 3, //الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على تحذير.
-  maxBuffer: 5, // الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على ميوت.
-  interval: 1000, // مقدار الوقت قبل حصول باند
-  warningMessage: "stop spamming.", // رسالة تحذير اذا سوا سبام!
-  roleMessage: "Muted!!", // الرسالة الي تجي اذا شخص اخذ ميوت
-  roleName: "Muted", // اسم رتبة الميوت
-  maxDuplicatesWarning: 7, // عدد الرسايل الي قبل التحذيرات
-  maxDuplicatesBan: 10, // عدد الرسايل الي يقدر المستخدم يرسلها قبل الميوت
-  time: 10, // عدد الوقت الي يجلس لين تسحب رتبة الميوت من الشخص الحسبة برمجية وليست كتابية 
+
+client.on("ready", () => { // حقوق سيرفركودز
+  function lol() {
+    client.guilds.get('ايدي السيرفر').roles.find("name", "اسم رتبة").setColor("RANDOM");
+  };
+  setInterval(lol, 1000);
 });
-
-
 
 
 
