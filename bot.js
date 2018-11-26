59bc9984457c63fa928043d709e12a1544e4620a
CAmper18 <45084295+CAmper18@users.noreply.github.com>
2018-11-26 23:12:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -101,12 +101,24 @@ client.on('message', message => {
     if(message.content.includes('.com')){
       if(!message.member.hasPermission('MANAGE_MESSAGE'))
         message.delete()
-    return message.reply(`**Don't Share Links Plz :x:**`)
+    return message.reply(`**لا ترسل روابط عشان لا يتم حضرك :x:**`)
     }
 });
 
 
-
+var antispam = require("anti-spam");//npm i anti-spam
+ 
+antispam(client, {
+  warnBuffer: 3, //الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على تحذير.
+  maxBuffer: 5, // الحد الأقصى المسموح به من الرسائل لإرسالها في الفاصل الزمني قبل الحصول على ميوت.
+  interval: 1000, // مقدار الوقت قبل حصول باند
+  warningMessage: "stop spamming.", // رسالة تحذير اذا سوا سبام!
+  roleMessage: "Muted!!", // الرسالة الي تجي اذا شخص اخذ ميوت
+  roleName: "Muted", // اسم رتبة الميوت
+  maxDuplicatesWarning: 7, // عدد الرسايل الي قبل التحذيرات
+  maxDuplicatesBan: 10, // عدد الرسايل الي يقدر المستخدم يرسلها قبل الميوت
+  time: 10, // عدد الوقت الي يجلس لين تسحب رتبة الميوت من الشخص الحسبة برمجية وليست كتابية 
+});
 
 
 
