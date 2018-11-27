66e1b205e614ed1f1b0bb2e94a6d3b5473d9b491
CAmper18 <45084295+CAmper18@users.noreply.github.com>
2018-11-27 11:38:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -172,7 +172,7 @@ client.on('message', msg => {
 
 client.on("message", (message) => {
     /// ABDOU
-   if (message.content.startsWith("-new")) {     /// ABDOU
+   if (message.content.startsWith("$new")) {     /// ABDOU
         const reason = message.content.split(" ").slice(1).join(" ");     /// ABDOU
         if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`لازم تسوي رتبة اسمها \`Support Team\` وتنطي البوت ادمنيتر حتا يقدر يسوي الرومات ويعدل برمشنات`);
         if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
